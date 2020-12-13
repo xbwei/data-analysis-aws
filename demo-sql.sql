f91b8736be57becf652b74c13f00c398bef8466b
Xuebin Wei <weixuebin@gmail.com>
2020-12-13 10:13:45 -0500

    Update demo-sql.sql

--- demo-sql.sql
+++ demo-sql.sql
@@ -9,3 +9,19 @@ CREATE TABLE public.student
     CONSTRAINT student_pkey PRIMARY KEY (s_id)
 )
 
+-- adding data
+
+insert into public.student (s_id,s_name,s_major,s_year)
+values ('1','s1','GS','2020');
+
+insert into public.student (s_id,s_name,s_major,s_year)
+values ('2','s2','IA','2020');
+
+insert into public.student (s_id,s_name,s_major,s_year)
+values ('3','s3','CS','2019');
+
+insert into public.student (s_id,s_name,s_major,s_year)
+values ('4','s4','GS','2019');
+
+insert into public.student (s_id,s_name,s_major,s_year)
+values ('5','s5','GS','2019');
