27e1122addb1e5d98fca2762e6807f3b21d62353
Xuebin Wei <weixuebin@gmail.com>
2020-12-13 10:23:48 -0500

    Update demo-sql.sql

--- demo-sql.sql
+++ demo-sql.sql
@@ -25,3 +25,30 @@ values ('4','s4','GS','2019');
 
 insert into public.student (s_id,s_name,s_major,s_year)
 values ('5','s5','GS','2019');
+
+-- querying data
+
+select * from public.student;
+
+select s_name,s_year from public.student
+order by s_year ASC;
+
+-- filtering data
+
+select s_major,s_name,s_year from public.student
+where s_major = 'GS'
+order by s_year ASC
+limit 1
+
+-- aggregate functions
+
+select count(s_id) as num_enroll
+from public.student
+where s_major = 'GS'
+
+
+-- grouping data
+
+select  s_major, count(s_id) as num_enroll
+from public.student
+group by s_major
